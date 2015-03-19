d0d9d54d4aa536cf13f8897a6cb243451ac58107
asaintorens <alexandre.saintorens@gmail.com>
2015-03-19 17:16:18 +0100

    added redis

---
+++ StalkerWorker/UserRedis.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StalkerWorker
+{
+    public class UserRedis 
+    {
+
+
+        public long Id { get; set; }
+      
+        public bool Facebook {get;set;}
+        public bool Twitter {get;set;}
+        public bool Google {get;set;}
+
+        public string username {get;set;}
+        public string email {get;set;}
+    }
+}
